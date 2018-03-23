a8adfe2c845cdfb42bbcdaf602574bfcd894bddf
Mykhailo Tymchyshyn <mykhailotymchyshyn@mykhailos-macbook-pro.local>
2018-03-23 16:28:42 +0200

    no changes...

--- Project19/Model/Path.swift
+++ Project19/Model/Path.swift
@@ -9,7 +9,7 @@
 import Foundation
 import MapKit
 
-struct Path {
+class Path {
     let startLocation: CLLocationCoordinate2D
     let startLocationDescription: String
     let endLocation: CLLocationCoordinate2D
